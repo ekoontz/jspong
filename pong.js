f23c7f3e41c15906a6f4fde2fb452c9a1d731841
Eugene Koontz <ekoontz@hiro-tan.org>
2009-07-18 13:05:03 -0700

    2 balls and more styles

--- pong.js
+++ pong.js
@@ -1,33 +1,48 @@
-var x = 100;
-var y = 100;
-
-var increment_x = 3;
-var increment_y = 1;
 
 function setup() {
- setTimeout("move_ball()", 10);
+    document.getElementById("ball1").style.left = "100px";
+    document.getElementById("ball1").style.top  = "200px";
+
+    document.getElementById("ball2").style.left = "150px";
+    document.getElementById("ball2").style.top  = "50px";
+
+    var increment_x_1 = 3;
+    var increment_y_1 = 1;
+
+    setTimeout("move_ball('ball1',"+increment_x_1+","+increment_y_1+")", 10);
+
+    var increment_x_2 = 1;
+    var increment_y_2 = 3;
+
+
+    setTimeout("move_ball('ball2',"+increment_x_2+","+increment_y_2+")", 10);
+
 }
 
-function move_ball() {
-  if ((x > 500) || (x < -50)) {
-    increment_x = increment_x * -1;
-  }
+function move_ball(ball_id,increment_x,increment_y) {
+    var x = document.getElementById(ball_id).style.left;
+    var y = document.getElementById(ball_id).style.top;
 
-  if ((y > 150) || (y < -100)) {
-    increment_y = increment_y * -1;
-  }
+    x = new Number(x.replace(/px/gi, ""));
+    y = new Number(y.replace(/px/gi, ""));
+ 
+    if ((x > 290) || (x < 0)) {
+	increment_x = increment_x * -1;
+    }
+    
+    if ((y > 390) || (y < 0)) {
+	increment_y = increment_y * -1;
+    }
+    
+    x = x + increment_x;
+    y = y + increment_y;
+ 
+/*    document.getElementById("show_x").innerHTML = increment_x;
+    document.getElementById("show_y").innerHTML = increment_y;*/
 
-  x = x + increment_x;
-  y = y + increment_y;
+    document.getElementById(ball_id).style.left = x+"px";
+    document.getElementById(ball_id).style.top = y+"px";
 
-  document.getElementById("show_x").innerHTML = x;
-  document.getElementById("show_y").innerHTML = y;
-
-  document.getElementById("ball").style.left = x+"px";
-  document.getElementById("ball").style.top = y+"px";
-  document.getElementById("ball").style.background = "yellow";
-
-  setTimeout("move_ball()",10);
-
+    setTimeout("move_ball('"+ball_id+"',"+increment_x+","+increment_y+")", 10);
 
 }
